50c9b0f184208697dc760509539fa3501747b813
Nicholas <jacksaintjack@gmail.com>
2018-06-12 14:58:55 -0500

    Learned to use push and unshift

---
+++ js/playlist.js
@@ -0,0 +1,8 @@
+var playList = []
+
+playList.push('I did It My Way');
+playList.push("Respect", "Imagine");
+playList.unshift('Born to Run');
+playList.unshift('Louie Louie', 'Maybellene');
+
+printList(playList);
